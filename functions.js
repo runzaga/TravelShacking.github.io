f12a822f94136fa52924fd5260b7c1776155276d
Rob <polunza@yahoo.com>
2020-12-08 14:33:36 -0700

    Update functions.js

--- functions.js
+++ functions.js
@@ -2,3 +2,12 @@
 function sendEmail(){
 	window.open('mailto:rob.unzaga@gmail.com?subject=Test&body=Hey Rob, How are you doing?')
 }
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
